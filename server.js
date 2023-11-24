37dc72daec120b3e2c3954ad5888aafa0f25513e
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-24 20:14:58 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require("path");
+const cors = require("cors")
+const bodyParser = require('body-parser');
+const customers = require('../router/customer');
+const server = express();
+
+//server
+const port = process.env.PORT || 3000;
+
+
+// morgan
+server.use(morgan('dev'));
+
+// body parser
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use('/customer', customers);
+
+// error
+server.use((req, res, next) => {
+  const error = new Error('Tidak Ditemukan');
+  error.status = 404;
+  next(error);
+});
+
+// Fuction buat handling error
+server.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+
+// Server berjalan
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
