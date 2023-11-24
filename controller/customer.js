1e76fa0896a5a347f3e8ee30070ac90862ebe1fa
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-24 20:14:19 +0700

    Create customer.js

---
+++ controller/customer.js
@@ -0,0 +1,24 @@
+const model = require('../model/index');
+const controller = {};
+
+// controler getall
+controller.getAll = async function (req, res) {
+    try {
+        const result = await model.customer.findAll();
+        if (result.length > 0) {
+            res.status(200).json({
+                message: "Get customer",
+                data: result
+            });
+        } else {
+            res.status(200).json({
+                message: "Tidak ada data",
+                data: []
+            });
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+};
