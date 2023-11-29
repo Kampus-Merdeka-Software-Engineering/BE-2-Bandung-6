92e4775cba373f292240c3e1aefe9b76acb34c3b
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-30 02:44:21 +0700

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,9 @@
 const customer = require('./customer');
+const layanan = require('./layanan');
 const controller = {};
 
 // controler customer
 controller.customer = customer;
+controller.layanan = layanan;
+
 module.exports = controller;
