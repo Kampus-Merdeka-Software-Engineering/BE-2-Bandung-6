42041346812ee869e3debfb1f97864556d54ba65
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-30 02:44:42 +0700

    Create layanan.js

---
+++ model/layanan.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
+
+// Buat database customer
+const Layanan = sequelize.define(
+  'tbl_layanan',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    kota_asal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    kota_tujuan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tanggal_keberangkatan: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    jam_keberangkatan: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    batas_tiket: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    harga_tiket: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Tabel telah berhasil dibuat.');
+  })
+  .catch((error) => {
+    console.error('Terjadi kesalahan saat mencoba membuat tabel:', error);
+  });
+
+module.exports = Layanan;
