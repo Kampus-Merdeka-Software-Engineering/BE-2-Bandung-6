bacbac67c17fa88d629c68985098a402b0620632
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-30 02:44:25 +0700

    Create layanan.js

---
+++ controller/layanan.js
@@ -0,0 +1,27 @@
+const model = require('../model/index');
+const controller = {};
+
+// controler create
+controller.createLayanan = async function (req, res) {
+    try {
+        const result = await model.customer.findAll();
+        if (result.length > 0) {
+            res.status(200).json({
+                message: "Get customer",
+                data: result
+            });
+        } else {
+            res.status(200).json({
+                message: "Tidak ada data",
+                data: []
+            });
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+};
+
+
+module.exports = controller;
