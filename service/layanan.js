bdf1ab3259ac298e69c82946e2ca8f687c9b5422
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-01 05:05:33 +0700

    Create layanan.js

---
+++ service/layanan.js
@@ -0,0 +1,34 @@
+const Layanan = require('../models/layanan');
+
+class LayananService {
+  //   async getAllLayanan() {
+  //     try {
+  //       const layanan = await Layanan.findAll();
+  //       return layanan;
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //   }
+
+  //   async getLayananById(id) {
+  //     try {
+  //       const layanan = await Layanan.findByPk(id);
+  //       return layanan;
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //   }
+
+//   async createLayanan(kota_asal, kota_tujuan, tanggal_keberangakatan, jam_keberangkatan, batas_tiket, harga_tiket) {
+//     try {
+//       const newLayanan = await Layanan.create(kota_asal, kota_tujuan, tanggal_keberangakatan, jam_keberangkatan, batas_tiket, harga_tiket);
+//       return newLayanan;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+
+// }
+
+// module.exports = new LayananService();
+}
