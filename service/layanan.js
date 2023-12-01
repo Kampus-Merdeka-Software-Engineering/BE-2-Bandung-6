0e94a7fde7a494c0ca32907289060e2e02ecebc7
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-01 20:32:49 +0700

    Update layanan.js

--- service/layanan.js
+++ service/layanan.js
@@ -1,34 +1,37 @@
-const Layanan = require('../models/layanan');
+const Layanan = require('../model/layanan');
+// const sequelize = require('../config/sequelize');
 
-class LayananService {
-  //   async getAllLayanan() {
-  //     try {
-  //       const layanan = await Layanan.findAll();
-  //       return layanan;
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   }
-
-  //   async getLayananById(id) {
-  //     try {
-  //       const layanan = await Layanan.findByPk(id);
-  //       return layanan;
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   }
-
-//   async createLayanan(kota_asal, kota_tujuan, tanggal_keberangakatan, jam_keberangkatan, batas_tiket, harga_tiket) {
-//     try {
-//       const newLayanan = await Layanan.create(kota_asal, kota_tujuan, tanggal_keberangakatan, jam_keberangkatan, batas_tiket, harga_tiket);
-//       return newLayanan;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
-// }
-
-// module.exports = new LayananService();
+async function getAllLayanan() {
+  try {
+    const result = await Layanan.findAll();
+    if (result.length === 0) {
+      throw new Error('Data tidak ditemukan.');
+    }
+    return result;
+  } catch (error) {
+    throw error;
+  }
 }
+
+async function getLayananById(id) {
+  try {
+    const result = await Layanan.findByPk(id);
+    if (result.length === 0) {
+      throw new Error('Data tidak ditemukan.');
+    }
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createLayanan(kota_asal, kota_tujuan, tanggal_keberangkatan, jam_keberangkatan, batas_tiket, harga_tiket) {
+  try {
+    const newLayanan = await Layanan.create(kota_asal, kota_tujuan, tanggal_keberangkatan, jam_keberangkatan, batas_tiket, harga_tiket);
+    return newLayanan;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {getAllLayanan, getLayananById, createLayanan};
