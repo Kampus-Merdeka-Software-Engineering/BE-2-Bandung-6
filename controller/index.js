5ce7177c0dc78c91a7d6c82df4206d9d36577690
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 01:24:02 +0700

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,14 @@
 const data_penumpang = require('./data_penumpang');
 const layanan = require('./layanan');
+const detail_pemesanan = require('./detail_pemesanan');
+const kota = require('./kota');
+const pembayaran = require('./pembayaran');
 const controller = {};
-
 
 controller.data_penumpang = data_penumpang;
 controller.layanan = layanan;
+controller.kota = kota;
+controller.detail_pemesanan = detail_pemesanan;
+controller.pembayaran = pembayaran;
 
 module.exports = controller;
