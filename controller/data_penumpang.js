919dab894180765bb4196cd2cdaf7f32cd916cb9
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 01:15:14 +0700

    Create data_penumpang.js

---
+++ controller/data_penumpang.js
@@ -0,0 +1,28 @@
+const model = require('../model/index');
+const controller = {};
+
+// controler getall
+// controller.getAll = async function (req, res) {
+//     try {
+//         const result = await model.customer.findAll();
+//         if (result.length > 0) {
+//             res.status(200).json({
+//                 message: "Get customer",
+//                 data: result
+//             });
+//         } else {
+//             res.status(200).json({
+//                 message: "Tidak ada data",
+//                 data: []
+//             });
+//         }
+//     } catch (error) {
+//         res.status(404).json({
+//             message: error.message
+//         });
+//     }
+// };
+
+// create customer
+
+module.exports = controller;
