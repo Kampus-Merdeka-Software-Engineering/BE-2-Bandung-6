bbb189fbbe7fed0e97e68683e889c75919742935
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 19:06:31 +0700

    Update kota.js

---
+++ service/kota.js
@@ -0,0 +1,37 @@
+const Kota = require('../model/kota');
+
+
+async function getAllKotaTravel() {
+  try {
+    const result = await Kota.findAll();
+    if (result.length === 0) {
+      throw new Error('Data tidak ditemukan.');
+    }
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function findByIdKotaTravel(id) {
+    try {
+      const result = await Kota.findByPk(id);
+      if (!result) {
+        throw new Error('Data tidak ditemukan.');
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+async function createKotaTravel(alamat_travel, kota_asal_travel, no_telepon_travel, gambar_travel) {
+  try {
+    const newKota = await Kota.create(alamat_travel, kota_asal_travel, no_telepon_travel, gambar_travel);
+    return newKota;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { getAllKotaTravel, findByIdKotaTravel, createKotaTravel };
