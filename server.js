02c115723a03923d3e2e3eb50f770c7cc1150ceb
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 19:06:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors")
 const bodyParser = require('body-parser');
 const server = express();
 const layananRouter = require('./router/layanan')
+const kotaRouter = require('./router/kota')
+const datapenumpangRouter= require('./router/data_penumpang')
 
 
 
@@ -15,6 +17,8 @@ server.use(express.json())
 
 //Router
 server.use("/layanan", layananRouter);
+server.use("/kota", kotaRouter)
+server.use("/datapenumpang", datapenumpangRouter)
 // morgan
 server.use(morgan('dev'));
 
