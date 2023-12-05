1da0013ca258dfe16e88d806a232ee7ffc63b408
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 19:04:08 +0700

    Update kota.js

---
+++ controller/kota.js
@@ -0,0 +1,46 @@
+const kotaService = require('../service/kota');
+
+// controler getall
+const getKotaTravel = async (req, res) => {
+  try {
+    const result = await kotaService.getAllKota();
+    res.json(result);
+  } catch (error) {
+    console.error('Gagal mendapatkan daftar kota travel:', error);
+    res.status(500).json({ error: 'Gagal mendapatkan daftar kota travel' });
+  }
+};
+// controler create
+const createKotaTravel = async (req, res) => {
+  try {
+    const { alamat_travel, kota_asal_travel, no_telepon_travel, gambar_travel } = req.body;
+    const newKotaTravel = await kotaService.createKota({ alamat_travel, kota_asal_travel, no_telepon_travel, gambar_travel });
+
+    res.status(201).json({
+      message: 'Kota travel berhasil dibuat',
+      data: newKotaTravel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Gagal membuat kota travel',
+      error: error.message,
+    });
+  }
+};
+const getKotaTravelWithId = async (req, res) => {
+    const kotaId = req.params.id;
+    try {
+      const kota = await kotaService.findByIdKota(kotaId);
+  
+      if (!kota) {
+        return res.status(404).json({ message: 'kota tidak ditemukan.' });
+      }
+  
+      return res.status(200).json(kota);
+    } catch (error) {
+      console.error('Terjadi kesalahan:', error);
+      return res.status(500).json({ message: 'Terjadi kesalahan internal.' });
+    }
+  };
+
+module.exports = { getKotaTravel, createKotaTravel, getKotaTravelWithId };
