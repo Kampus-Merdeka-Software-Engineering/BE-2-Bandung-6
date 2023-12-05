35b8be2f4bef4331b970456870d5b19952caeb39
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-05 19:06:21 +0700

    Update layanan.js

--- router/layanan.js
+++ router/layanan.js
@@ -4,6 +4,7 @@ router.use(express.json());
 const layananController = require('../controller/layanan')
 
 router.get('/api/getlayanan', layananController.getLayanan)
+router.get('/:id', layananController.getLayananWithId)
 router.post('/api/postlayanan', layananController.createLayanan)
 
 module.exports = router
