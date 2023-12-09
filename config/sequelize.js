3e3fcfeb0faee12b16d87452feb0315e8a5b19aa
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-09 08:32:06 +0700

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'mysql',
 });
 sequelize
