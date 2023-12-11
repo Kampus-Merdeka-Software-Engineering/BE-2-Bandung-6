dc8ebe87405f9ff85ed0b292742c4fb1acc0bb88
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-11 13:23:51 +0700

    Update data_penumpang.js

--- router/data_penumpang.js
+++ router/data_penumpang.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
-const datapenumpangController = require('../controller/data_penumpang')
+const datapenumpangController = require('../controller/data_penumpang');
 
-router.get('/api/getdatapenumpang', datapenumpangController.getDataPenumpang)
-router.get('/:id', datapenumpangController.getDataPenumpangWithId)
-router.post('/api/postdatapenumpang',datapenumpangController.createDataPenumpang)
+router.get('/api/getdatapenumpang', datapenumpangController.getDataPenumpang);
+router.get('/id/:id', datapenumpangController.getDataPenumpangWithId);
+router.get('/:alamatEmail', datapenumpangController.getDataByEmail);
+router.post('/api/postdatapenumpang', datapenumpangController.createDataPenumpang);
 
-module.exports = router
+module.exports = router;
